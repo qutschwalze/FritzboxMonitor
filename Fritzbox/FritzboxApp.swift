36808d6061f063a37960cd5b7fdab5d9a452a5cd
Jens Heddrich <jh@7oz.de>
2025-12-24 08:48:47 +0100

    Initial Commit

---
+++ Fritzbox/FritzboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FritzboxApp.swift
+//  Fritzbox
+//
+//  Created by Sledge on 24.12.25.
+//
+
+import SwiftUI
+
+@main
+struct FritzboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
